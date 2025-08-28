99949835b0d72cf6203f30998442090cf565a7db
SangerForCode <viikkii.college@gmail.com>
2025-08-29 01:01:35 +0530

    1:01

--- labs/fraction.java
+++ labs/fraction.java
@@ -2,14 +2,14 @@ package labs;
 
 public class fraction{
     public static void main(String args[]){
-        int[] z = new int[5];
-        z[0]=1;
-        z[1]=2;
-        z[2]=3;
-        z[3]=14;
-        z[4]=15;
-        int x = z[3];
-        int[] y = {1,2,3,4};
-        System.out.println(z[4]);
+        String a = new String("nier");
+        String b = "brother";
+        String c = a+b;
+        String d = c.substring(0,4) + "niju";
+
+        
+        System.out.println(b.charAt(5));
+        System.out.println(c);
+        System.out.println(d);
          }
 }
